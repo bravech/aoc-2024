df62e280efd5e968e725c3f40a8fa2e9d1671604
Charlie Phillips <cgp246@gmail.com>
2024-12-25 20:03:14 +0000

    day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,55 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    for line in input.lines() {
+        let (l, r) = line.split_once("   ").unwrap();
+        left.push(l.parse().unwrap());
+        right.push(r.parse().unwrap());
+    }
+
+    left.sort();
+    right.sort();
+    let mut ret = 0;
+    for (l, r) in left.iter().zip(right.iter()) {
+        ret += (l - r).abs();
+    }
+    return Some(ret.try_into().unwrap())
+
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    for line in input.lines() {
+        let (l, r) = line.split_once("   ").unwrap();
+        left.push(l.parse().unwrap());
+        right.push(r.parse().unwrap());
+    }
+
+    return Some(
+        left.iter().map(|l|
+            right.iter().filter(|&n| *n == *l).sum::<i32>()
+        ).sum()
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
